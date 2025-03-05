283b1b8655692730d9b0341524e8d53d1730320f
manifesto <manifestomixx@gmail.com>
2025-03-05 16:37:37 +0100

    pricing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,17 @@ import FeaturesSection from "@/components/key-features";
 import Pricing from "@/components/pricing";
 import { Newsletter } from "@/components/newsletter";
 import { Faq } from "../components/faq";
+import Hero from "@/components/hero";
+
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <FeaturesSection />
-      <Pricing />
       <CreateAd />
       <Pricing />
       <Faq />
-      <Newsletter />
-      <Footer />
       <Newsletter />
       <Footer />
     </main>

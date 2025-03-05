486b0b28f17cb06304b19ba3392bd5e5bab2fcc6
manifesto <manifestomixx@gmail.com>
2025-03-05 15:36:35 +0100

    updated: pricing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,19 @@
 import { CreateAd } from "@/components/create-ad";
 import Footer from "@/components/footer";
 import FeaturesSection from "@/components/key-features";
-<<<<<<< HEAD
 import Pricing from "@/components/pricing";
-=======
-import NewsletterComponent from "@/components/newsletter";
+import { Newsletter } from "@/components/newsletter";
 import { Faq } from "../components/faq";
->>>>>>> 75eb841d401d65fd2396134e851a946d9554530a
 
 export default function Home() {
   return (
     <main>
       <FeaturesSection />
-<<<<<<< HEAD
       <Pricing />
-=======
       <CreateAd />
       <Faq />
-      <NewsletterComponent />
->>>>>>> 75eb841d401d65fd2396134e851a946d9554530a
+      <Newsletter />
+      <Footer />
     </main>
   );
 }
--- src/components/pricing.tsx
+++ src/components/pricing.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Nunito } from "next/font/google";
 import clsx from "clsx";
+import { IoCheckmark } from "react-icons/io5";
+import { FaDollarSign } from "react-icons/fa6";
+
 
 const nunito = Nunito({
     variable: '--font-nunito',
@@ -12,41 +15,53 @@ export default function Pricing() {
     const pricing = [
         {id: 1, title: 'Starter', description: "Get a few features to start your Adgen-ai journey.", price: "Free", features: ['5 ads per month', 'Watermarked exports', 'Limited selection of templates', 'Standard resolution only (720p)']},
 
-        {id: 2, title: 'Basic', description: "Get full usage of the generator ", price: '$30', features: ['Unlimited generated ads', 'No Watermarked exports', 'Access to premium templates', 'Exports in HD quality (1080p)']},
+        {id: 2, title: 'Basic', description: "Get full usage of the generator ", price: '30', features: ['Unlimited generated ads', 'No Watermarked exports', 'Access to premium templates', 'Exports in HD quality (1080p)']},
 
-        {id: 3, title: 'Teams', description: "A full package for a 3-4 membered team", price: '$50', features: ['Unlimited generated ads', 'Share with team members', 'Team brand kit integration', '4K resolution & custom export options']},
+        {id: 3, title: 'Teams', description: "A full package for a 3-4 membered team", price: '50', features: ['Unlimited generated ads', 'Share with team members', 'Team brand kit integration', '4K resolution & custom export options']},
     ];
 
     return (
-        <div className={clsx("w-full py-16 px-4 md:px-6 lg:px-8 relative overflow-hidden", nunito.variable)}>
+        <div className={clsx("w-full py-16 px-4 md:px-6 lg:px-8 relative overflow-hidden bg-black", nunito.variable)}>
             <div className='max-w-7xl mx-auto text-center space-y-8'>
 
                 <div className='space-y-4'>
                     <h3 className='text-[#B800B8] font-bold text-lg'>UNLOCK THE FULL POTENTIAL OF AI-ADGEN</h3>
-                    <h1 className='text-4xl font-semibold'>Pricing Plans</h1>
-                    <p className='text-gray-600'> Generate your ads for free and pay as you grow</p>
+                    <h1 className='text-4xl font-semibold text-white'>Pricing Plans</h1>
+                    <p className='text-white'> Generate your ads for free and pay as you grow</p>
                 </div>
 
                 {/* Card */}
-                <div className='grid grid-cols-1 md:grid-cols-3 gap-8 mt-5 justify-center'>
+                <div className='grid grid-cols-1 md:grid-cols-3 gap-4 mt-5 text-start'>
                     {pricing.map((plan, index) => (
                         <div key={plan.id} className={clsx('p-5 rounded-2xl w-[381px] h-[539px] flex flex-col justify-between', index === 1 ? 'bg-[#B800B8] text-white' : 'bg-white text-black')}>
-                            <div className='space-y-5'>
-                                <div>
+                            <div className='space-y-5 flex-grow'>
+                                <div className={clsx('border-b-[0.2px] pb-3 h-[9rem]', index === 1 ? 'border-white' : 'border-gray-300 text-[#B800B8]')}>
+
+                                {/* Plan title  */}
+                                <div className='mb-3'>
                                     <h2 className={clsx('text-2xl font-semibold', index === 1 ? 'text-white' : 'text-[#B800B8]')}>{plan.title}</h2>
-                                    <p className='text-gray-500'>{plan.description}</p>
+                                    <p className={clsx('text-sm w-[15rem] mb-5', index === 1 ? "text-gray-200 h-[2.5rem]" : "text-gray-400")}>{plan.description}</p>
                                 </div>
-                                <h3 className={clsx('border-b-[0.5px] pb-3 text-2xl font-semibold', index === 1 ? 'border-white' : 'border-gray-300 text-[#B800B8]')}>{plan.price} {plan.price !== "Free" && (
-                                        <span className="text-base">/mo</span>
+
+                                <div>
+                                    
+                                </div>
+                                <h3 className='text-2xl font-semibold flex items-center'>{plan.price !== "Free" && <FaDollarSign />}{plan.price} {plan.price !== "Free" && (
+                                        <span className="text-base font-light mt-2 pl-1">/mo</span>
                                     )}</h3>
-                                <ul className='space-y-2'>
-                                    {plan.features.map((feature, index) => (
-                                        <li key={index} className="flex items-center gap-2">✓ {feature}</li>
+                                </div>
+
+                                <ul className='space-y-5 pt-5'>
+                                    {plan.features.map((feature, featureIndex) => (
+                                        <li key={featureIndex} className="flex items-center gap-2">
+                                        <IoCheckmark className={clsx('text-sm', index === 1 ? 'text-white' : 'text-black')} />
+                                        {feature}
+                                    </li>
                                     ))}
                                 </ul>
                             </div>
-                            <div className='flex justify-center mt-8'>
-                                <button className={clsx('p-3 rounded-lg font-medium ', index === 1 ? 'bg-white text-[#B800B8]' : 'bg-[#B800B8] text-white')}>Choose plan</button>
+                            <div className='flex justify-center mt-'>
+                                <button className={clsx('p-4 rounded-md font-medium cursor-pointer', index === 1 ? 'bg-white text-[#B800B8]' : 'bg-[#B800B8] text-white')}>Choose plan</button>
                             </div>
                         </div>
                     ))}

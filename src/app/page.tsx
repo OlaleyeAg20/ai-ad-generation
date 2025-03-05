ae8f663f274741d02f9eccc45bda85dd29e7316a
olaleyeag.20 <olaleyeag.20@student.funaab.edu.ng>
2025-03-05 14:59:35 +0100

    add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import FeaturesSection from "@/components/key-features";
 import { Faq } from "../components/faq";
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
     <main>
+      <Hero />
       <FeaturesSection />
       <Faq/>
     </main>

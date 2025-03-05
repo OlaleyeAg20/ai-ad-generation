29a2fe314fe04a5c024ae3d1cd046e3a51409dc5
Marv_lous <155150860+Marvellous-Udoye@users.noreply.github.com>
2025-03-05 11:48:22 +0100

    feat: add FAQ section with mock data and interactive accordion components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import FeaturesSection from "@/components/key-features";
+import { Faq } from "../components/faq";
 
 export default function Home() {
   return (
     <main>
       <FeaturesSection />
+      <Faq/>
     </main>
   );
 }
